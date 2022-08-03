ef1b80eac700aae8d7cdff920136ab1511d2a707
DivyakumarBhagat <108863887+DivyakumarBhagat@users.noreply.github.com>
2022-08-03 22:33:50 +0530

    Update header.js

--- static/components/header.js
+++ static/components/header.js
@@ -1,5 +1,5 @@
-export const header =
-`<style>
+export const header = () => {
+return `<style>
 .header {overflow: hidden;background-color: #f1f1f1;padding: 20px 10px;}
 .header a {float: left;color: black;text-align: center;padding: 12px;text-decoration: none;font-size: 18px; line-height: 25px;border-radius: 4px;}
 .header a.logo {font-size: 25px;font-weight: bold;}
@@ -12,3 +12,4 @@ export const header =
 }
 </style>
 <div class="header"><a href="#default" class="logo">CompanyLogo</a><div class="header-right"><a class="active" href="#home">Home</a><a href="#contact">Contact</a><a href="#about">About</a></div></div>`;
+};
