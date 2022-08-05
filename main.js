289bc0aad1b760cc7fb3982ce1e8276ab7e09850
DivyakumarBhagat <108863887+DivyakumarBhagat@users.noreply.github.com>
2022-08-05 14:06:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var callable ={name: "",
 add: function(name){this.name = name;},
 updatebyId: function(id){if(this.name){document.getElementById(id).innerHTML = this.name;}}};
 
-funtion test(met,id)
+function test(met,id)
 {
 callable.add(met);
 callable.updatebyId(id);
