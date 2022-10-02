01a03bdf67304495c447de9deb2a31800f5a9a46
DivyakumarBhagat <108863887+DivyakumarBhagat@users.noreply.github.com>
2022-10-02 15:22:00 +0530

    Create essentials.js

---
+++ maj/essentials.js
@@ -0,0 +1,16 @@
+export function nav() { return `<ul class="lst"><li><a href=''>Home</a></li><li><a href=''>About</a></li><li><a href=''>Portfolio</a></li><li><a href=''>Contact</a></li></ul>`; }
+export function ham() { return `<div class="ham"><span class="bar"></span><span class="bar"></span><span class="bar"></span></div>`;}
+export function logo() { return `<h1><a href=''>Flexbox</a></h1>`;}
+
+export function impLinks() { return `<ul><h3>Essential Links</h3><li><a href=''>Home</a></li><li><a href=''>About Us</a></li><li><a href=''>FAQs</a></li><li><a href=''>Contact Us</a></li><li><a href=''>Community n Forum</a></li><li><a href=''>Careers</a></li><li><a href=''>Support us</a></li></ul>`;}
+export function exploreCat() { return `<ul><h3>Explore Categories</h3><li><a href=''>Earth</a></li><li><a href=''>Humans</a></li><li><a href=''>Jupiter</a></li><li><a href=''>Sun</a></li><li><a href=''>Moon</a></li><li><a href=''>History</a></li><li><a href=''>Geography</a></li><li><a href=''>Living Organisms</a></li></ul>`;}
+export function solu() { return `<ul><h3>Solutions</h3><li><a href=''>Sales</a></li><li><a href=''>Project management</a></li><li><a href=''>Workforce</a></li><li><a href=''>Ecommerce</a></li><li><a href=''>Finance</a></li></ul>`;}
+export function legal() { return `<ul><h3>Legal Links</h3><li><a href=''>Disclaimers</a></li><li><a href=''>Cookies</a></li><li><a href=''>Web Accessibility</a></li><li><a href=''>Privacy Policy</a></li><li><a href=''>Terms n Conditions</a></li><li><a href=''>Sitemap</a></li></ul>`;}
+export function workwithus() { return `<ul><h3>Work With Us</h3><li><a href=''>Author</a></li><li><a href=''>Advertise</a></li></ul>`;}
+export function intouch() { return `<ul><h3>In Touch</h3><li><a href=''>FB</a><a href=''>TW</a><a href=''>LI</a><a href=''>IG</a><a href=''>YT</a><a href=''>RD</a><a href=''>GH</a><a href=''>M</a></li></ul>`;}
+
+export function about() { return `<ul><h3>About</h3><li>Ifitztrue is one of the best scifi n paranormal activity investigation site to help many people like us to learn about latest technologies being used in paranormal activity n also provide shoppingfor ur fav instuments</li></ul>`;}
+export function contact() { return `<ul><h3>Contact Us</h3><li>Mozilla Foundation,</li><li>331 E Evelyn Ave,</li><li>Mountain View, CA 94041,</li><li>USA</li><li><a href=''>+1 5555551212</a></li><li><a href=''>email@example.com</a></li></ul>`;}
+export function newsletter() { return `<ul><h3>Newsletter</h3><li><form action="#"><li><input type="text" placeholder="Full Name" name="name" required></li><li><input type="email" placeholder="Email address" name="mail" required></li><li></li><li><input type="submit" value="Subscribe"></li></form></li></ul>`;}
+
+export function endfooter() { return `<p>copyright &copy; 2021-2022 <a href=''>If Itz True</a>. All Rights Reseverd</p>`;}
