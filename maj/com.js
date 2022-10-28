5e1d00a23292dced20ddd4284fbe69bbd0199a09
DivyakumarBhagat <108863887+DivyakumarBhagat@users.noreply.github.com>
2022-10-28 14:15:20 +0530

    Update com.js

--- maj/com.js
+++ maj/com.js
@@ -1,8 +1,8 @@
 
 import { nav,logo,impLinks,exploreCat,solu,legal,workwithus,intouch,about,contact,newsletter,endfooter } from './essentials.js';
 
-function header() {return '<header class="header var1">'+logo()+''+nav()+'</header>';}
-function footer() {return '<footer class="footer"><section>${impLinks()}${exploreCat()}${solu()}${legal()}${workwithus()}${intouch()}</section><section>${about()}${contact()}${newsletter()}</section><section>${endfooter()}</section></footer>`;}
+function header() {return `<header class="header var1">${logo()}${nav()}</header>`;}
+function footer() {return '<footer class="footer"><section>${impLinks()}${exploreCat()}${solu()}${legal()}${workwithus()}${intouch()}</section><section>${about()}${contact()}${newsletter()}</section><section>${endfooter()}</section></footer>';}
 
 
 
