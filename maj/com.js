5224879dc27d97cdae4c4056fa704f7605587da1
DivyakumarBhagat <108863887+DivyakumarBhagat@users.noreply.github.com>
2022-10-30 16:50:40 +0530

    Update com.js

--- maj/com.js
+++ maj/com.js
@@ -1,8 +1,11 @@
 
 import { nav,logo,impLinks,exploreCat,solu,legal,workwithus,intouch,about,contact,newsletter,endfooter } from './essentials.js';
 
-function header() {return `<header class="header var1">${logo()}${nav()}</header>`;}
-function footer() {return '<footer class="footer"><section>${impLinks()}${exploreCat()}${solu()}${legal()}${workwithus()}${intouch()}</section><section>${about()}${contact()}${newsletter()}</section><section>${endfooter()}</section></footer>';}
+
+
+function header() {return `<header class="var1">${logo()}${nav()}</header>`;}
+function footer() {return `<footer><section>${about()}${impLinks()}${exploreCat()}${solu()}${legal()}${workwithus()}$
+{intouch()}${contact()}${newsletter()}</section><section>${endfooter()}</section></footer>`;}
 
 
 
@@ -11,5 +14,5 @@ function execution()
 document.getElementById("header").outerHTML = header();
 document.getElementById("footer").outerHTML = footer();
 }
-//window.addEventListener('load', (event) =>{execution();});
-window.addEventListener('DOMContentLoaded', (event) =>{execution();});
+window.addEventListener('load', (event) =>{execution();});
+//window.addEventListener('DOMContentLoaded', (event) =>{execution();});
