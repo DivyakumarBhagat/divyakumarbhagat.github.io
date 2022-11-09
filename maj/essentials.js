f17418da9f720c7b3bc7070c1b18dc25581451f9
DivyakumarBhagat <108863887+DivyakumarBhagat@users.noreply.github.com>
2022-11-09 14:48:32 +0530

    Update essentials.js

--- maj/essentials.js
+++ maj/essentials.js
@@ -1,29 +1,45 @@
 "use strict"
 
 function acc(a,b="") {return `<li><a href="${a.link}">${a.name}</a>${b}</li>`;}
-export function nav() {
- var json =   [
-{"link":"/", "name":"Home"},
-{"link":"#", "name":"About","sub":[{"link":"#", "name":"Portfolio"},{"link":"#", "name":"Others"} ]},
-{"link":"#", "name":"Contacts"}
-];
-var a ="";var b ="";
+function gen(json) {
+ var a ="";var b ="";
 for (let i in json) {if (json[i].hasOwnProperty("sub")) {json[i].sub.forEach((a) => {b = b + acc(a);});b = `<ul>${b}</ul>`;}a = a + acc(json[i], b);b="";}
-   return `<nav>${a}</nav>`;  
+}
+export function nav() {
+ var json =   [{"link":"/", "name":"Home"},{"link":"#", "name":"About","sub":[{"link":"#", "name":"Portfolio"},{"link":"#", "name":"Others"} ]},{"link":"#", "name":"Contacts"}];
+   return `<nav>${gen(json)}</nav>`;  
 }               
                        
 
 export function logo() { return '<section class="a"><img src="https://flexbox.ninja/assets/images/logo.svg" alt="Flexbox.ninja"><a href="#">If Itz True</a></section>';}
 
-export function impLinks() { return `<ul><h3>Essential Links</h3><li><a href=''>Home</a></li><li><a href=''>About Us</a></li><li><a href=''>FAQs</a></li><li><a href=''>Contact Us</a></li><li><a href=''>Community n Forum</a></li><li><a href=''>Careers</a></li><li><a href=''>Support us</a></li></ul>`;}
-export function exploreCat() { return `<ul><h3>Explore Categories</h3><li><a href=''>Earth</a></li><li><a href=''>Humans</a></li><li><a href=''>Jupiter</a></li><li><a href=''>Sun</a></li><li><a href=''>Moon</a></li><li><a href=''>History</a></li><li><a href=''>Geography</a></li><li><a href=''>Living Organisms</a></li></ul>`;}
-export function solu() { return `<ul><h3>Solutions</h3><li><a href=''>Sales</a></li><li><a href=''>Project management</a></li><li><a href=''>Workforce</a></li><li><a href=''>Ecommerce</a></li><li><a href=''>Finance</a></li></ul>`;}
-export function legal() { return `<ul><h3>Legal Links</h3><li><a href=''>Disclaimers</a></li><li><a href=''>Cookies</a></li><li><a href=''>Web Accessibility</a></li><li><a href=''>Privacy Policy</a></li><li><a href=''>Terms n Conditions</a></li><li><a href=''>Sitemap</a></li></ul>`;}
-export function workwithus() { return `<ul><h3>Work With Us</h3><li><a href=''>Author</a></li><li><a href=''>Advertise</a></li></ul>`;}
-export function intouch() { return `<ul><h3>In Touch</h3><li><a href=''>FB</a><a href=''>TW</a><a href=''>LI</a><a href=''>IG</a><a href=''>YT</a><a href=''>RD</a><a href=''>GH</a><a href=''>M</a></li></ul>`;}
+export function impLinks() {
+ var json =   [
+{"link":"/", "name":"Home"},{"link":"#", "name":"About Us"},{"link":"#", "name":"FAQs"},{"link":"#", "name":"Contact Us"},{"link":"#", "name":"Community n Forum"},{"link":"#", "name":"Careers"},{"link":"#", "name":"Support us"}];
+ return `<ul><h3>Essential Links</h3>${gen(json)}</ul>`;}
+
+export function exploreCat() { 
+  var json =   [{"link":"#", "name":"Earth"},{"link":"#", "name":"Humans"},{"link":"#", "name":"Jupiter"},{"link":"#", "name":"Sun"},{"link":"#", "name":"Moon"},{"link":"#", "name":"History"},{"link":"#", "name":"Geography"},{"link":"#", "name":"Living Organisms"}];
+ return `<ul><h3>Explore Categories</h3>${gen(json)}</ul>`;}
+
+export function solu() { 
+  var json =   [{"link":"#", "name":"Sales"},{"link":"#", "name":"Project management"},{"link":"#", "name":"Workforce"},{"link":"#", "name":"Ecommerce"},{"link":"#", "name":"Finance"}];
+ return `<ul><h3>Solutions</h3>${gen(json)}</ul>`;}
+
+export function legal() { 
+ var json =   [{"link":"#", "name":"Disclaimers"},{"link":"#", "name":"Cookies"},{"link":"#", "name":"Web Accessibility"},{"link":"#", "name":"Privacy Policy"},{"link":"#", "name":"Terms n Conditions"},{"link":"#", "name":"Sitemap"}];
+ return `<ul><h3>Legal Links</h3>${gen(json)}</ul>`;}
+
+export function workwithus() { 
+var json =   [{"link":"#", "name":"Author"},{"link":"#", "name":"Advertise"}];
+ return `<ul><h3>Work With Us</h3>${gen(json)}</ul>`;}
+
+export function intouch() { 
+ var json =   [{"link":"#", "name":"FB"},{"link":"#", "name":"TW"},{"link":"#", "name":"LI"},{"link":"#", "name":"IG"},{"link":"#", "name":"YT"},{"link":"#", "name":"RD"},{"link":"#", "name":"GH"},{"link":"#", "name":"M"}];
+ return `<ul><h3>In Touch</h3>${gen(json)}</ul>`;}
 
 export function about() { return `<ul class="a"><h3>About</h3><li>Ifitztrue is one of the best scifi n paranormal activity investigation site to help many people like us to learn about latest technologies being used in paranormal activity n also provide shoppingfor ur fav instuments</li></ul>`;}
 export function contact() { return `<ul class="a"><h3>Contact Us</h3><li>Mozilla Foundation,</li><li>331 E Evelyn Ave,</li><li>Mountain View, CA 94041,</li><li>USA</li><li><a href=''>+1 5555551212</a></li><li><a href=''>email@example.com</a></li></ul>`;}
 export function newsletter() { return `<ul class="a"><h3>Newsletter</h3><li><form action="#"><li><input type="text" placeholder="Full Name" name="name" required></li><li><input type="email" placeholder="Email address" name="mail" required></li><li></li><li><input type="submit" value="Subscribe"></li></form></li></ul>`;}
 
-export function endfooter() { return `<p>copyright &copy; 2021-2022 <a href=''>If Itz True</a>. All Rights Reseverd</p>`;}
+export function endfooter() { return `<p>copyright &copy; 2021-2022 <a href='/'>If Itz True</a>. All Rights Reseverd</p>`;}
