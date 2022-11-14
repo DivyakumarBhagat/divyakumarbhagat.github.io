3d7a1908855198b75587bbb5e5920886549d0333
DivyakumarBhagat <108863887+DivyakumarBhagat@users.noreply.github.com>
2022-11-14 15:24:15 +0530

    Update essentials.js

--- maj/essentials.js
+++ maj/essentials.js
@@ -1,8 +1,7 @@
 "use strict"
 
-
-var sitename = `If Itz True`;
-var logoset = `<img src="https://flexbox.ninja/assets/images/logo.svg" alt="Flexbox.ninja"><a href="#">${sitename}</a>`;
+function site() {return 'if Itz True';}
+function logoset() { return `<img src="https://flexbox.ninja/assets/images/logo.svg" alt="${site()} Logo"><a href="#">${site()}</a>`; }
 
 function acc(a,b="") {return `<li><a href="${a.link}">${a.name}</a>${b}</li>`;}
 function gen(json) {
@@ -54,7 +53,7 @@ export function intouch() {
  return met("In Touch",gens(json));}
 
 export function about() { 
- return met(`<h3>${logoset}</h3>`,`<li>Ifitztrue is one of the best scifi n paranormal activity investigation site to help many people like us to learn about latest technologies being used in paranormal activity n also provide shoppingfor ur fav instuments</li>`);}
+ return met(`<h3>${logoset()}</h3>`,`<li>Ifitztrue is one of the best scifi n paranormal activity investigation site to help many people like us to learn about latest technologies being used in paranormal activity n also provide shoppingfor ur fav instuments</li>`);}
 
 export function contact() { 
  return met("Contact Us",`<li>Mozilla Foundation,</li><li>331 E Evelyn Ave,</li><li>Mountain View, CA 94041,</li><li>USA</li><li><a href=''>+1 5555551212</a></li><li><a href=''>email@example.com</a></li>`);}
@@ -63,4 +62,4 @@ export function newsletter() {
  return met("Newsletter",`<form action="#"><p>Subscribe to our newsletter to get our latest news.</p><input type="text" placeholder="Full Name" name="name" required><input type="email" name="mail" placeholder="Enter email address" required><input type="submit" value="Subscribe"></form>`);}
 
 export function endfooter() { 
- return met("",`<p>copyright &copy; 2021-2022 <a href='/'>${sitename}</a>. All Rights Reseverd</p>`);}
+ return met("",`<p>copyright &copy; 2021-2022 <a href='/'>${site()}</a>. All Rights Reseverd</p>`);}
