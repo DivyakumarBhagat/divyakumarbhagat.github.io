77cac5811672bc48581610e244e120e4d1e4c215
DivyakumarBhagat <108863887+DivyakumarBhagat@users.noreply.github.com>
2022-11-20 14:03:48 +0530

    Update essentials.js

--- maj/essentials.js
+++ maj/essentials.js
@@ -8,8 +8,7 @@ function gen(json) {var a ="";var b ="";for (let i in json) {if (json[i].hasOwnP
 
 
 function met(title,content) { return `<div><h3>${title}</h3><ul>${content}</ul></div>`;}
-function mets(title,content) { return `<div class="aa"><h3>${title}</h3><ul>${content}</ul></div>`;}
-function meta(title,content) { return `<div class="a"><h3>${title}</h3><ul>${content}</ul></div>`;}
+function meta(title,content) { return `<div class="a"><h3>${title}</h3>${content}</div>`;}
 
 export function nav() {
  var json =   [{"link":"/", "name":"Home"},{"link":"#", "name":"About","sub":[{"link":"#", "name":"Portfolio"},{"link":"#", "name":"Others"} ]},{"link":"#", "name":"Contacts"}];
@@ -33,16 +32,16 @@ var json =   [{"link":"#", "name":"Author"},{"link":"#", "name":"Advertise"}];
  return met("Work With Us",gen(json));}
 export function intouch() { 
  var json =   [{"link":"#", "name":"FB"},{"link":"#", "name":"TW"},{"link":"#", "name":"LI"},{"link":"#", "name":"IG"},{"link":"#", "name":"YT"},{"link":"#", "name":"RD"},{"link":"#", "name":"GH"},{"link":"#", "name":"M"}];
- return gen(json);}
+ return `<ul>${gen(json)}</ul>`;}
 
 export function about() { 
- return meta(`<h3>${logoset()}</h3>`,`<li>Ifitztrue is one of the best scifi n paranormal activity investigation site to help many people like us to learn about latest technologies being used in paranormal activity n also provide shoppingfor ur fav instuments</li>`);}
+ return meta(`${logoset()}`,`<p>Ifitztrue is one of the best scifi n paranormal activity investigation site to help many people like us to learn about latest technologies being used in paranormal activity n also provide shoppingfor ur fav instuments</p>`);}
 
 export function contact() { 
  return met("Contact Us",`<li>Mozilla Foundation,</li><li>331 E Evelyn Ave,</li><li>Mountain View, CA 94041,</li><li>USA</li><li><a href=''>+1 5555551212</a></li><li><a href=''>email@example.com</a></li>`);}
 
 export function newsletter() { 
- return met("Newsletter",`<form action="#"><p>Subscribe to our newsletter to get our latest news.</p><input type="text" placeholder="Full Name" name="name" required><input type="email" name="mail" placeholder="Enter email address" required><input type="submit" value="Subscribe"></form>`);}
+ return meta("Newsletter",`<form action="#"><p>Subscribe to our newsletter to get our latest news.</p><input type="text" placeholder="Full Name" name="name" required><input type="email" name="mail" placeholder="Enter email address" required><input type="submit" value="Subscribe"></form>`);}
 
 
 export function searchsomething() {
